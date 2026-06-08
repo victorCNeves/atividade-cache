45175a0133b4deca427172ac7fc73ea2a9f4234c
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-06-08 18:17:40 -0300

    feat: add cache middleware

---
+++ src/middleware/cache.js
@@ -0,0 +1,9 @@
+import expressRedisCache from "express-redis-cache";
+
+export default expressRedisCache({
+  client: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    expire: 60,
+  },
+});
