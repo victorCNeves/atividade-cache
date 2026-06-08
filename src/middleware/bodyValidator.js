99390f695536b33715e3bbda14d30edbfe12249c
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-06-08 18:23:58 -0300

    feat: add bodyValidator

---
+++ src/middleware/bodyValidator.js
@@ -0,0 +1,27 @@
+export default (req, res, next) => {
+  const { title, content, author } = req.body;
+  const errors = {};
+
+  if (typeof title !== "string") {
+    errors.title = "O título deve ser uma string.";
+  } else if (title.trim() === "string") {
+    errors.title = "O título é obrigatório.";
+  }
+
+  if (typeof content !== "string") {
+    errors.content = "O conteúdo deve ser uma string.";
+  } else if (content.trim() === "string") {
+    errors.content = "O conteúdo é obrigatório.";
+  }
+  if (typeof author !== "string") {
+    errors.author = "O autor deve ser uma string.";
+  } else if (author.trim() === "string") {
+    errors.author = "O autor é obrigatório.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json({ errors: erros });
+  }
+
+  next();
+};
