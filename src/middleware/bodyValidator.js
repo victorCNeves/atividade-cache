e0613f9333c606c5f46b6a450b64ebffc406b8ca
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-06-08 21:35:45 -0300

    fix: if da validação estava comparando o trim com "string"

--- src/middleware/bodyValidator.js
+++ src/middleware/bodyValidator.js
@@ -4,23 +4,23 @@ export default async (req, res, next) => {
 
   if (typeof title !== "string") {
     errors.title = "O título deve ser uma string.";
-  } else if (title.trim() === "string") {
+  } else if (title.trim() === "") {
     errors.title = "O título é obrigatório.";
   }
 
   if (typeof content !== "string") {
     errors.content = "O conteúdo deve ser uma string.";
-  } else if (content.trim() === "string") {
+  } else if (content.trim() === "") {
     errors.content = "O conteúdo é obrigatório.";
   }
   if (typeof author !== "string") {
     errors.author = "O autor deve ser uma string.";
-  } else if (author.trim() === "string") {
+  } else if (author.trim() === "") {
     errors.author = "O autor é obrigatório.";
   }
 
   if (Object.keys(errors).length > 0) {
-    return res.status(400).json({ errors: erros });
+    return res.status(400).json({ errors: errors });
   }
 
   next();
