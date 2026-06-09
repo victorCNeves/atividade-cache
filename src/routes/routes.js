398565fe6b74e0df320c988a87b94b61f0156e97
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-06-08 22:56:33 -0300

    refactor: utilização da nova middleware de cache

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,16 +3,11 @@ import postController from "../controller/postController.js";
 import idValidator from "../middleware/idValidator.js";
 import queryValidator from "../middleware/queryValidator.js";
 import bodyValidator from "../middleware/bodyValidator.js";
-import cache from "../config/cache.js";
+import cache from "../middleware/cache.js";
 
 const router = express.Router();
 
-router.get(
-  "/posts",
-  queryValidator,
-  cache.route({ name: "/posts" }),
-  postController.getAll,
-);
+router.get("/posts", queryValidator, cache, postController.getAll);
 router.get("/posts/:id", idValidator, postController.getById);
 router.post("/posts", bodyValidator, postController.create);
 
